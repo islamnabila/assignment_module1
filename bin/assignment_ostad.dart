43ba0b640dd4358b8a0f53a56109c8edaae89bc8
islamnabila <81310064+islamnabila@users.noreply.github.com>
2023-08-02 21:20:08 +0600

    first commit

---
+++ bin/assignment_ostad.dart
@@ -0,0 +1,5 @@
+import 'package:assignment_ostad/assignment_ostad.dart' as assignment_ostad;
+
+void main(List<String> arguments) {
+  print('Hello world: ${assignment_ostad.calculate()}!');
+}
---
+++ lib/first_assignment.dart
@@ -0,0 +1,18 @@
+void main(){
+
+  const int a = 32;
+  const int b = 12;
+
+  int add = (a+b);
+  int sub = (a-b);
+  int mul = (a*b);
+  double div = (a/b);
+  int mod = (a%b);
+
+  print("Addition: $a + $b = $add");
+  print("Subtraction: $a - $b = $sub");
+  print("Multiplication: $a * $b = $mul");
+  print("Division: $a / $b = $div");
+  print("Modulus: $a % $b = $mod");
+
+}
